0677599ea6bcb4f6901699581c2004abbb63acd5
massine2001 <137456591+massine2001@users.noreply.github.com>
2024-11-17 13:06:18 +0100

    Update Footer.jsx

--- client/components/Footer.jsx
+++ client/components/Footer.jsx
@@ -3,7 +3,7 @@ import { UserContext } from '../pages/appContext';
 
 function Footer() {
   const { user } = useContext(UserContext);
-
+//{user && user.email && (user.username ? user.username : user.email.split('@')[0])}
   return (
     <footer className="c-footer">
       <div className="flex justify-between">
@@ -11,7 +11,7 @@ function Footer() {
         <p className="text-gray-100 text-sm">Personal Project  /  Recipes for mother</p>
         {user && (
           <div className="text-green-500 text-sm">
-            {user && user.email}
+            connecté
           </div>
         )}
         {!user && <div className="text-gray-500 text-sm">Guest user</div>}
